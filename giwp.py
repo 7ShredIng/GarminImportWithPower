64fa1822f599c9dd2274ccc2257abe86a505325e
7ShredIng <sickguttural@directbox.com>
2017-08-20 22:20:58 +0200

    add vitual power to tcx files
    adding virtual power to tcx files with batch file calling
    tcx_vpower_0_3.py

--- giwp.py
+++ giwp.py
@@ -6,7 +6,9 @@ assert (sys.version_info.major < 3),"ERROR: currently python2.7 only!"
 GarminDeviceFolder = "/media/sick/GARMIN/GARMIN/ACTIVITY"
 ImportDir = "/media/sick/Training/FenixImport/tcx"
 VirtualPowerDir = "/media/sick/Training/FenixImport/vpower"
-FitToTcxDir = "~/FIT-to-TCX/batch.sh"
+FitToTcx = "~/FIT-to-TCX/batch.sh"
+TcxVPower = "~/Documents/tcx_vpower_0_3/batch.sh"
+Mass = 95
 
 print("-- check if directories are available")
 assert (os.path.isdir(GarminDeviceFolder)),"ERROR: dir does not exist"
@@ -18,13 +20,16 @@ importCommand = '/usr/bin/rsync --progress -r -v -s %s %s' % (GarminDeviceFolder
 p = subprocess.Popen(importCommand, shell=True).wait()
 
 print("-- convert fit files to tcx")
-convertFitToTcxCommand = '%s %s/*.FIT' % (FitToTcxDir, ImportDir)
-print(convertFitToTcxCommand)
+convertFitToTcxCommand = '%s %s/*.FIT' % (FitToTcx, ImportDir)
 p = subprocess.Popen(convertFitToTcxCommand, shell=True).wait()
 
 print("-- calculate and add virtual power to tcx files")
+addVPowerCommand = '%s %s/*.tcx --kalman --mass %s' % (TcxVPower, ImportDir, Mass)
+p = subprocess.Popen(addVPowerCommand, shell=True).wait()
 
 print("-- sync vpower files to Golden Cheetah import directory")
+exportGCFolderCommand = '/usr/bin/rsync --progress -r -v -s %s %s' % (ImportDir, VirtualPowerDir)
+p = subprocess.Popen(exportGCFolderCommand, shell=True).wait()
 
 print("-- import to Golden Cheetah")
 
