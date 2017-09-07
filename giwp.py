e202bdb1cbed844a8ec16ac8759d7ba1ca93b8e3
7ShredIng <sickguttural@directbox.com>
2017-09-07 22:29:59 +0200

    corrected script for all steps
    all steps of the script are now working:
    - copy FIT files from device to folder
    - convert FIT files to TCX files
    - calculate and add virtual power to TCX files
    - copy TCX files with added virtual power to Golden Cheetah import
    directory
    TODO: batch import to Golden Cheetah

--- giwp.py
+++ giwp.py
@@ -3,9 +3,9 @@ import subprocess
 
 assert (sys.version_info.major < 3),"ERROR: currently python2.7 only!"
 
-GarminDeviceFolder = "/media/sick/GARMIN/GARMIN/ACTIVITY"
-ImportDir = "/media/sick/Training/FenixImport/tcx"
-VirtualPowerDir = "/media/sick/Training/FenixImport/vpower"
+GarminDeviceFolder = "/media/sick/GARMIN/GARMIN/ACTIVITY/"
+ImportDir = "/media/sick/Training/FenixImport/tcx/"
+VirtualPowerDir = "/media/sick/Training/FenixImport/vpower/"
 FitToTcx = "~/FIT-to-TCX/batch.sh"
 TcxVPower = "~/Documents/tcx_vpower_0_3/batch.sh"
 Mass = 95
@@ -20,15 +20,18 @@ importCommand = '/usr/bin/rsync --progress -r -v -s %s %s' % (GarminDeviceFolder
 p = subprocess.Popen(importCommand, shell=True).wait()
 
 print("-- convert fit files to tcx")
-convertFitToTcxCommand = '%s %s/*.FIT' % (FitToTcx, ImportDir)
+convertFitToTcxCommand = '%s %s*.FIT' % (FitToTcx, ImportDir)
+print('%s' % convertFitToTcxCommand)
 p = subprocess.Popen(convertFitToTcxCommand, shell=True).wait()
 
 print("-- calculate and add virtual power to tcx files")
-addVPowerCommand = '%s %s/*.tcx --kalman --mass %s' % (TcxVPower, ImportDir, Mass)
+addVPowerCommand = '%s %s*.tcx' % (TcxVPower, ImportDir)
+print('%s' % addVPowerCommand)
 p = subprocess.Popen(addVPowerCommand, shell=True).wait()
 
 print("-- sync vpower files to Golden Cheetah import directory")
-exportGCFolderCommand = '/usr/bin/rsync --progress -r -v -s %s %s' % (ImportDir, VirtualPowerDir)
+exportGCFolderCommand = '/usr/bin/rsync --progress -r -v -s --include=\'vpower_*\' --include=\'*/\' --exclude=\'*\' %s %s' % (ImportDir, VirtualPowerDir)
+print('%s' % exportGCFolderCommand)
 p = subprocess.Popen(exportGCFolderCommand, shell=True).wait()
 
 print("-- import to Golden Cheetah")
