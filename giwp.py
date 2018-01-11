afbe1a169f359528fea9271c8a7beac51c5bb026
CodeShredder726b <codeshredder726b@gmail.com>
2018-01-11 22:48:22 +0100

    Make class, minor cleanups.

--- giwp.py
+++ giwp.py
@@ -6,66 +6,75 @@ from bunch import bunchify
 
 assert (sys.version_info.major < 3),"ERROR: currently python2.7 only!"
 
-#os.system('mount -L GARMIN /mnt/GarminFenix3')
+class GIWP:
 
-## Read configuration from yaml.
-## [Folders]
-## - path to Garmin device where the FIT files are located
-## - Import directory to where the files are processed
-## - Export directory to where copy the virtual power files
-## [Scripts]
-## - batch script: fit to tcx conversion
-## - batch script: vpower conversion
-def readConfig():
-    with open('.config') as conf:
-        configuration = yaml.safe_load(conf)
-    
-    b = bunchify(configuration)
-    GarminDeviceFolder  = b.Folders.GarminDevActivities
-    ImportDir           = b.Folders.ImportDir
-    VirtualPowerDir     = b.Folders.ExportDir
-    FitToTcx            = b.Scripts.FitToTcx
-    TcxVPower           = b.Scripts.TcxVPower
-    conf.close()
+    GarminDeviceFolder = ""
+    ImportDir = ""
+    VirtualPowerDir = ""
+    FitToTcx = ""
+    TcxVPower = ""
 
-def checkPaths():
-    print("-- check if directories are available")
-    assert (os.path.isdir(GarminDeviceFolder)),"ERROR: dir does not exist"
-    assert (os.path.isdir(ImportDir)),"ERROR: dir does not exist"
-    assert (os.path.isdir(VirtualPowerDir)),"ERROR: dir does not exist"
+    #os.system('mount -L GARMIN /mnt/GarminFenix3')
 
-def importFitFiles():
-    print("-- sync fit files to ImportDir")
-    importCommand = '/usr/bin/rsync --progress -rvsh --ignore-existing %s %s' % (GarminDeviceFolder, ImportDir)
-    p = subprocess.Popen(importCommand, shell=True).wait()
+    ## Read configuration from yaml.
+    ## [Folders]
+    ## - path to Garmin device where the FIT files are located
+    ## - Import directory to where the files are processed
+    ## - Export directory to where copy the virtual power files
+    ## [Scripts]
+    ## - batch script: fit to tcx conversion
+    ## - batch script: vpower conversion
+    def readConfig(self):
+        with open('.config') as conf:
+            configuration = yaml.safe_load(conf)
+        
+        b = bunchify(configuration)
+        GIWP.GarminDeviceFolder  = b.Folders.GarminDevActivities
+        GIWP.ImportDir           = b.Folders.ImportDir
+        GIWP.VirtualPowerDir     = b.Folders.ExportDir
+        GIWP.FitToTcx            = b.Scripts.FitToTcx
+        GIWP.TcxVPower           = b.Scripts.TcxVPower
+        conf.close()
 
-def convertFitToTcx():
-    print("-- convert fit files to tcx")
-    convertFitToTcxCommand = '%s %s*.FIT' % (FitToTcx, ImportDir)
-    print('%s' % convertFitToTcxCommand)
-    p = subprocess.Popen(convertFitToTcxCommand, shell=True).wait()
+    def checkPaths(self):
+        print("-- check if directories are available")
+        assert (os.path.isdir(GIWP.GarminDeviceFolder)),"ERROR: dir does not exist"
+        assert (os.path.isdir(GIWP.ImportDir)),"ERROR: dir does not exist"
+        assert (os.path.isdir(GIWP.VirtualPowerDir)),"ERROR: dir does not exist"
 
-def calculateVirtualPower():
-    print("-- calculate and add virtual power to tcx files")
-    addVPowerCommand = '%s %s*.tcx' % (TcxVPower, ImportDir)
-    print('%s' % addVPowerCommand)
-    p = subprocess.Popen(addVPowerCommand, shell=True).wait()
+    def importFitFiles(self):
+        print("-- sync fit files to ImportDir")
+        importCommand = '/usr/bin/rsync --progress -rvsh --ignore-existing %s %s' % (GIWP.GarminDeviceFolder, GIWP.ImportDir)
+        p = subprocess.Popen(importCommand, shell=True).wait()
 
-def exportVPowerFiles():
-    print("-- sync vpower files to Golden Cheetah import directory")
-    exportGCFolderCommand = '/usr/bin/rsync --progress -rvsh --include=\'vpower_*\' --include=\'*/\' --exclude=\'*\' %s %s' % (ImportDir, VirtualPowerDir)
-    for f in glob.glob(ImportDir + "vpower*.tcx"):
-        os.remove(f)
-    print('%s' % exportGCFolderCommand)
-    p = subprocess.Popen(exportGCFolderCommand, shell=True).wait()
+    def convertFitToTcx(self):
+        print("-- convert fit files to tcx")
+        convertFitToTcxCommand = '%s %s*.FIT' % (GIWP.FitToTcx, GIWP.ImportDir)
+        print('%s' % convertFitToTcxCommand)
+        p = subprocess.Popen(convertFitToTcxCommand, shell=True).wait()
+
+    def calculateVirtualPower(self):
+        print("-- calculate and add virtual power to tcx files")
+        addVPowerCommand = '%s %s*.tcx' % (GIWP.TcxVPower, GIWP.ImportDir)
+        print('%s' % addVPowerCommand)
+        p = subprocess.Popen(addVPowerCommand, shell=True).wait()
+
+    def exportVPowerFiles(self):
+        print("-- sync vpower files to Golden Cheetah import directory")
+        exportGCFolderCommand = '/usr/bin/rsync --progress -rvsh --include=\'vpower_*\' --include=\'*/\' --exclude=\'*\' %s %s' % (GIWP.ImportDir, GIWP.VirtualPowerDir)
+        for f in glob.glob(GIWP.ImportDir + "vpower*.tcx"):
+            os.remove(f)
+        print('%s' % exportGCFolderCommand)
+        p = subprocess.Popen(exportGCFolderCommand, shell=True).wait()
 
 def main():
-    readConfig()
-    checkPaths()
-    importFitFiles()
-    convertFitToTcx()
-    calculateVirtualPower()
-    exportVPowerFiles()
+    giwp = GIWP()
+    giwp.readConfig()
+    giwp.checkPaths()
+    giwp.importFitFiles()
+    giwp.convertFitToTcx()
+    giwp.calculateVirtualPower()
+    giwp.exportVPowerFiles()
 
 if __name__ == "__main__":
     main()
